344ac34b48d6f171e38dd9302a86fec7c5f67639
vinnyfs89 <viniciusfesil@gmail.com>
2018-12-17 14:20:40 -0200

    [UPDATE] Utilização de rotas

--- node/src/main.js
+++ node/src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import './plugins/vuetify'
+import router from './router';
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
