cb44eeee192bcf1812d242c9728bc3a6d43762f9
vinnyfs89 <viniciusfesil@gmail.com>
2018-12-17 14:15:56 -0200

    [UPDATE] Adição de componente Home

---
+++ node/src/components/Home.vue
@@ -0,0 +1,54 @@
+<template>
+    <v-container fluid>
+        <v-card>
+            <v-toolbar light>
+                <v-bottom-nav
+                        :active.sync="bottomNav"
+                        :color="color"
+                        :value="true">
+                    <v-btn dark value="/sortear-numeros" to="/sortear-numeros">
+                        <span>Sortear Números</span>
+                        <v-icon>devices</v-icon>
+                    </v-btn>
+
+                    <v-btn dark value="/sortear-nomes" to="/sortear-nomes">
+                        <span>Sortear Nomes</span>
+                        <v-icon>settings_system_daydream</v-icon>
+                    </v-btn>
+
+                    <v-btn dark value="/sortear-nomes" to="/sortear-nomes">
+                        <span>Sortear Amigo Secreto</span>
+                        <v-icon>chat</v-icon>
+                    </v-btn>
+
+                </v-bottom-nav>
+            </v-toolbar>
+            <v-card-text heigth="300px">
+                <router-view />
+            </v-card-text>
+        </v-card>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                bottomNav: '/sortear-numeros'
+            }
+        },
+
+        computed: {
+            color() {
+                switch (this.bottomNav) {
+                    case '/sortear-numeros':
+                        return 'blue-grey'
+                    case '/sortear-nomes':
+                        return 'brown'
+                    case '/sortear-nomes':
+                        return 'teal'
+                }
+            }
+        }
+    }
+</script>
