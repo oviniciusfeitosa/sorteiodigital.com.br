12f5845aade8d01d56c435521adac016aadcf3b4
vinnyfs89 <viniciusfesil@gmail.com>
2018-12-17 14:19:05 -0200

    [UPDATE] Adição de componente `NaoEncontrado`

---
+++ node/src/components/NaoEncontrado.vue
@@ -0,0 +1,9 @@
+<template>
+    <v-container fluid>
+        <v-layout column justify-center>
+            <v-card flat>
+                <h1>P&aacute;gina n&atilde;o localizada ;( </h1>
+            </v-card>
+        </v-layout>
+    </v-container>
+</template>
