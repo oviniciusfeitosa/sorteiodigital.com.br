d949f8222f3171bcd8112f884746e2b1c043cde7
vinnyfs89 <viniciusfesil@gmail.com>
2018-12-17 14:19:24 -0200

    [UPDATE] Adição de componente `SorteadorDeNomes`

---
+++ node/src/components/SorteadorDeNomes.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-container>
+        <v-layout
+                text-xs-center
+                wrap>
+            <v-flex xs12>
+                <h1>Em desenvolvimento.</h1>
+            </v-flex>
+
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({})
+  }
+</script>
+
+<style>
+
+</style>
