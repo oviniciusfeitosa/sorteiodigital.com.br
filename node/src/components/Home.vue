05dbed69942db55265dbd4cb70da188e967a9fd8
vinnyfs89 <viniciusfesil@gmail.com>
2018-12-18 00:45:00 -0200

    [UPDATE] Adição de função de sortear Números

--- node/src/components/Home.vue
+++ node/src/components/Home.vue
@@ -1,6 +1,10 @@
 <template>
     <v-container fluid>
+
         <v-card>
+            <v-card-text heigth="300px">
+                <router-view />
+            </v-card-text>
             <v-toolbar light>
                 <v-bottom-nav
                         :active.sync="bottomNav"
@@ -23,9 +27,17 @@
 
                 </v-bottom-nav>
             </v-toolbar>
-            <v-card-text heigth="300px">
-                <router-view />
-            </v-card-text>
+            <!--<v-expansion-panel popout>-->
+                <!--<v-expansion-panel-content-->
+                        <!--v-for="(item,i) in 5"-->
+                        <!--:key="i"-->
+                <!--&gt;-->
+                    <!--<div slot="header">Item</div>-->
+                    <!--<v-card>-->
+                        <!--<v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</v-card-text>-->
+                    <!--</v-card>-->
+                <!--</v-expansion-panel-content>-->
+            <!--</v-expansion-panel>-->
         </v-card>
     </v-container>
 </template>
--- node/src/components/SorteadorDeNumeros.vue
+++ node/src/components/SorteadorDeNumeros.vue
@@ -1,26 +1,35 @@
 <template>
     <v-container>
-        <v-layout
-                text-xs-center
-                wrap>
-            <v-flex xs12>
-                <v-text-field
-                        type="number"
-                        label="De?"
-                        box
-                        v-model="numeroInicial"
-                ></v-text-field>
-                 -
-                <v-text-field
-                        type="number"
-                        label="Até?"
-                        box
-                        v-model="numeroFinal"
-                ></v-text-field>
-                    {{randomNumber()}}
+        <v-card-text>
+            <v-layout
+                    text-xs-center
+                    class="text-xs-center"
+                    wrap>
+                <v-flex md1>
+                    De
+                    <v-text-field
+                            type="number"
+                            label="De?"
+                            box
+                            v-model="numeroInicial"></v-text-field>
+                </v-flex>
+                <v-divider class="mx-3"
+                           inset
+                           vertical></v-divider>
+                <v-flex md1>
+                    Até
+                    <v-text-field type="number"
+                                  label="Até?"
+                                  box
+                                  v-model="numeroFinal"></v-text-field>
+                </v-flex>
+            </v-layout>
+            <v-flex md3>
+                <v-card-title primary-title v-if="numeroSorteado != '' && numeroSorteado != null">
+                    <div class="headline">O número sorteado foi: <b>{{numeroSorteado}}</b></div>
+                </v-card-title>
             </v-flex>
-
-        </v-layout>
+        </v-card-text>
     </v-container>
 </template>
 
@@ -30,16 +39,31 @@
       return {
         numeroInicial: '',
         numeroFinal: '',
+        numeroSorteado: '',
       };
     },
-    methods : {
-      randomNumber : function(){
-        if(this.numeroInicial != '' && this.numeroFinal != '') {
-            if(this.numeroInicial > this.numeroFinal) {
-                return Math.floor(Math.random() * (this.numeroFinal - this.numeroInicial + this.numeroInicial)) + this.numeroInicial;
-            }
-            return Math.floor(Math.random() * (this.numeroInicial - this.numeroFinal + this.numeroFinal)) + this.numeroFinal;
+    watch: {
+      numeroInicial() {
+        this.numeroSorteado = this.sortearNumeros();
+      },
+      numeroFinal() {
+        this.numeroSorteado = this.sortearNumeros();
+      },
+    },
+    methods: {
+      sortearNumeros: function () {
+        this.numeroSorteado = '';
+        if (this.numeroInicial != '' && this.numeroFinal != '') {
+          let numeroInicial = parseInt(this.numeroInicial, 10);
+          let numeroFinal = parseInt(this.numeroFinal);
+          if (numeroInicial > numeroFinal) {
+            return this.randomizarNumeros(numeroFinal, numeroInicial);
+          }
+          return this.randomizarNumeros(numeroInicial, numeroFinal);
         }
+      },
+      randomizarNumeros(valorMinimo, valorMaximo) {
+        return Math.round(Math.random() * (valorMaximo - valorMinimo) + valorMinimo);
       }
     }
   }
