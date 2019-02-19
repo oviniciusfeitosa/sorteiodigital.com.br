58325f482d9cc865371905dcd7caa7d90b8202c5
vinnyfs89 <viniciusfesil@gmail.com>
2019-02-19 18:55:31 -0300

    [UPDATE] Adição de função inicial para adição de vários nomes.

--- node/src/components/SorteadorDeNomes.vue
+++ node/src/components/SorteadorDeNomes.vue
@@ -1,10 +1,20 @@
 <template>
     <v-container>
-        <v-layout
-                text-xs-center
-                wrap>
+        <v-layout text-xs-center>
             <v-flex xs12>
-                <h1>Em desenvolvimento.</h1>
+                <v-layout align-center justify-center row fill-height>
+                    <v-flex md12
+                            :key="plataforma.title"
+                            :v-for="grupoNome in this.grupoNomes">
+                        <v-text-field type="text"
+                                      label="Nome"
+                                      v-model="grupoNome.nome"
+                                      box></v-text-field>
+                    </v-flex>
+                </v-layout>
+                <v-layout align-center justify-center row fill-height>
+                    <v-btn @click="adicionarElemento()"></v-btn>
+                </v-layout>
             </v-flex>
 
         </v-layout>
@@ -13,7 +23,24 @@
 
 <script>
   export default {
-    data: () => ({})
+    data() {
+      return {
+        grupoNomes: [{
+          nome: '',
+        }],
+      };
+    },
+    methods() {
+      adicionarElemento() {
+        this.grupoNomes.push({
+          nome: ''
+        });
+      },
+
+    },
+    mounted() {
+
+    }
   }
 </script>
 
